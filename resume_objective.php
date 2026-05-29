283e88d87cc6083f24cd63a26849b971da59903d
Sebastian <francissebastianpertudo12@gmail.com>
2026-05-30 00:24:07 +0800

    fix SA2 folder structure

--- resume_objective.php
+++
@@ -1,2 +0,0 @@
-<h4 class="section-title">• Career Objective</h4>
-<p>Dedicated IT professional seeking to enhance my technical expertise and operational efficiency within a fast-paced development team.</p> 

c5b9566eb24a7ef77bfde2231b862d45dd488bbd
Sebastian <francissebastianpertudo12@gmail.com>
2026-05-30 00:12:20 +0800

    adding sa2 from my laptop

---
+++ resume_education.php
@@ -0,0 +1,4 @@
+<h4 class="section-title">• Educational Attainment Page</h4>
+<p><strong>FEU - Technology</strong></p>
+<p>Bachelor of Science in Information Technology with Specialization in Web and App Development</p>
+<p style="font-style: italic; color: var(--text-muted); margin-top: 4px;">CCSMA's Consistent Dean's Lister Academic Honors</p>
